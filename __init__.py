571fff533e3d333dc70f4bfc72428df1ca235257
teamshortcut <3aaron3@gmail.com>
2015-05-30 14:58:13 +0100

    Moved to flask

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
+from flask import Flask, render_template
 import xml.etree.ElementTree as ET
 import urllib.request
 
+app = Flask(__name__)
+
 url = "https://data.tfl.gov.uk/tfl/syndication/feeds/stations-facilities.xml"
 
+@app.route("/")
 def get_data():
     with urllib.request.urlopen(url) as response:
         html = response.read()    
-    data = ""
+    stations = []
     data2 = html
     root = ET.fromstring(data2)
 
     for station in root.iter("station"):
+        data = ""
         data += station.find("name").text#.decode("utf-8")
         facilities = station.find("facilities")
         data += "Has Toilets:"
@@ -19,7 +24,10 @@ def get_data():
         for facility in facilities:
             if facility.attrib["name"] == "Toilets":
                 data += facility.text#.decode("utf-8")
-        data += "\n"
-    return data
+        stations.append(data)
+    return render_template("index.html", stations=stations)
 
-print(get_data())
+#print(get_data())
+#input()
+
+app.run(debug=True)
